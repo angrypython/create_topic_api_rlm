7553d250c06981d6a0bbddbcaa8b140efae0655a
sergey <sergey_sivakov_84@mail.ru>
2022-11-15 20:40:58 +0300

    3

--- main.py
+++ main.py
@@ -2,38 +2,35 @@ import requests
 import urllib3
 import argparse
 import datetime
-#param_keys = ('url', "token", "action", "ak_topic", "ak_replication_factor", "ak_partitions", "ak_security", "ak_port", "ak_topic_config", "user_email", "env_type")
-
 def get_params_run():
+    
     mainparser = argparse.ArgumentParser(description="For create topics")
     subparsers = mainparser.add_subparsers(title='subcommands', description='valid subcommands', help='description')
 
     head = subparsers.add_parser("head", description="Heads")
     head.add_argument("-url", dest="url", default='https://rlm.sigma.ru')
     head.add_argument("-token", dest="token", default='token')
-    #heads = vars(head.parse_args())
-
-    parser = subparsers.add_parser("parser", description="Parser")
-    parser.add_argument("-url", dest="url", default='https://rlm.sigma.ru')
-    parser.add_argument("-token", dest="token", default='token')
-    parser.add_argument("-action", dest="action", default='aktag_create_topic')
-    parser.add_argument("-ak_topic", dest="ak_topic", default='test2api')
-    parser.add_argument("-ak_replication_factor", dest="ak_replication_factor", default='1')
-    parser.add_argument("-ak_partitions", dest="ak_partitions", default='1')
-    parser.add_argument("-ak_security", dest="ak_security", default='SSL')
-    parser.add_argument("-ak_port", dest="ak_port", default='9093')
-    parser.add_argument("-ak_topic_config", dest="ak_topic_config", default='[]')
-    parser.add_argument("-user_email", dest="user_email", default='user@example.com')
-    parser.add_argument("-env_type", dest="env_type", default='TEST')
-    #params = vars(parser.parse_args())
-    #[params.pop(key) for key in ['url', 'token']]
+    
+    param = subparsers.add_parser("parser", description="Parser")
+    param.add_argument("-action", dest="action", default='aktag_create_topic')
+    param.add_argument("-ak_topic", dest="ak_topic", default='test2api')
+    param.add_argument("-ak_replication_factor", dest="ak_replication_factor", default='1')
+    param.add_argument("-ak_partitions", dest="ak_partitions", default='1')
+    param.add_argument("-ak_security", dest="ak_security", default='SSL')
+    param.add_argument("-ak_port", dest="ak_port", default='9093')
+    param.add_argument("-ak_topic_config", dest="ak_topic_config", default='[]')
+    param.add_argument("-user_email", dest="user_email", default='user@example.com')
+    param.add_argument("-env_type", dest="env_type", default='TEST')
 
     item = subparsers.add_parser("item", description="Items")
-    item.add_argument("-invsvm_ci_svm", dest="invsvm_ci_svm", default='https://rlm.sigma.ru')
-    item.add_argument("-invsvm_ip", dest="invsvm_ip", default='token')
+    item.add_argument("-invsvm_ci_svm", dest="invsvm_ci_svm", default='CI04340141')
+    item.add_argument("-invsvm_ip", dest="invsvm_ip", default='10.56.165.201')
+
+    heads = vars(head.parse_args())
+    params = vars(param.parse_args())
     items = vars(item.parse_args())
 
-    #return [heads, params, items]
+    return [heads, params, items]
 
 def post_to_api(url, head,  param):
     urllib3.disable_warnings()
