330da654b120c8b62f508a729161480452e81df2
sergey <sergey_sivakov_84@mail.ru>
2022-11-15 20:30:05 +0300

    2

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import datetime
 #param_keys = ('url', "token", "action", "ak_topic", "ak_replication_factor", "ak_partitions", "ak_security", "ak_port", "ak_topic_config", "user_email", "env_type")
 
 def get_params_run():
-    head = argparse.ArgumentParser(description="For create topics")
+    mainparser = argparse.ArgumentParser(description="For create topics")
+    subparsers = mainparser.add_subparsers(title='subcommands', description='valid subcommands', help='description')
+
+    head = subparsers.add_parser("head", description="Heads")
     head.add_argument("-url", dest="url", default='https://rlm.sigma.ru')
     head.add_argument("-token", dest="token", default='token')
-    heads = vars(head.parse_args())
+    #heads = vars(head.parse_args())
 
-    parser = argparse.ArgumentParser(description="For create topics")
+    parser = subparsers.add_parser("parser", description="Parser")
     parser.add_argument("-url", dest="url", default='https://rlm.sigma.ru')
     parser.add_argument("-token", dest="token", default='token')
     parser.add_argument("-action", dest="action", default='aktag_create_topic')
@@ -22,15 +25,15 @@ def get_params_run():
     parser.add_argument("-ak_topic_config", dest="ak_topic_config", default='[]')
     parser.add_argument("-user_email", dest="user_email", default='user@example.com')
     parser.add_argument("-env_type", dest="env_type", default='TEST')
-    params = vars(parser.parse_args())
-    [params.pop(key) for key in ['url', 'token']]
+    #params = vars(parser.parse_args())
+    #[params.pop(key) for key in ['url', 'token']]
 
-    item = argparse.ArgumentParser(description="For create topics")
+    item = subparsers.add_parser("item", description="Items")
     item.add_argument("-invsvm_ci_svm", dest="invsvm_ci_svm", default='https://rlm.sigma.ru')
     item.add_argument("-invsvm_ip", dest="invsvm_ip", default='token')
     items = vars(item.parse_args())
 
-    return [heads, params, items]
+    #return [heads, params, items]
 
 def post_to_api(url, head,  param):
     urllib3.disable_warnings()
