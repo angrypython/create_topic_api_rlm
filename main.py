375c6ec1081f29ffcf195b8241fd55f73b85be81
angrypython <73248415+angrypython@users.noreply.github.com>
2022-11-16 23:21:25 +0300

    Update main.py
    last

--- main.py
+++ main.py
@@ -2,44 +2,53 @@
 import requests
 import urllib3
 import argparse
-#import datetime
+import datetime
 import json
-def get_params_run():
-    mainparser = argparse.ArgumentParser(description="For create topics")
-    subparsers = mainparser.add_subparsers(title='subcommands', description='valid subcommands', help='description')
-
-    head = subparsers.add_parser("head", description="Heads")
-    head.add_argument("-url", dest="url", default='https://rlm.delta.sbrf.ru/api/tasks/')
-    head.add_argument("-authorization", dest="Authorization", default='Token 8ecf4351b8926bca001cc7453618c52443968bfaea3ae4ef755583b48f973989')
-    head.add_argument("-accept", dest="accept", default='application/json')
-    head.add_argument("-content_type", dest="Content-Type", default='application/json')
-
-    param = subparsers.add_parser("param", description="Param")
-    param.add_argument("-action", dest="action", default='aktag_create_topic,aktag_modify_topic')
-    param.add_argument("-ak_topic", dest="ak_topic", default='test4api')
-    param.add_argument("-ak_replication_factor", dest="ak_replication_factor", type=int, default=1)
-    param.add_argument("-ak_partitions", dest="ak_partitions", type=int, default=1)
-    param.add_argument("-ak_security", dest="ak_security", default='SSL')
-    param.add_argument("-ak_port", dest="ak_port", type=int, default='9093')
-    param.add_argument("-ak_topic_config", dest="ak_topic_config", default=[])
-    param.add_argument("-user_email", dest="user_email", default='user@example.com')
-    param.add_argument("-env_type", dest="env_type", default='TEST')
-
-    when_start = subparsers.add_parser("item", description="Items")
-    when_start.add_argument("-start_at", dest="start_at", default='now')
-    when_start.add_argument("-service", dest="service", default='createtopicKafka')
-    when_start.add_argument("-datetime", dest="datetime", default='2022-11-16T07:25:20.488Z')
-
-    item = subparsers.add_parser("item", description="Items")
-    item.add_argument("-invsvm_ci_svm", dest="invsvm_ci_svm", default='CI04340141')
-    item.add_argument("-invsvm_ip", dest="invsvm_ip", default='10.56.165.201')
-
-    heads = vars(head.parse_args())
-    when = vars(when_start.parse_args())
-    params = vars(param.parse_args())
-    items = vars(item.parse_args())
-
+def split_dict(in_dict):
+    cor_heads=('url', 'Authorization', 'accept', 'Content-Type')
+    cor_params=('action', 'ak_topic', 'ak_replication_factor', 'ak_partitions', 'ak_security', 'ak_port', 'ak_topic_config', 'user_email', 'env_type')
+    cor_when=('start_at', 'service', 'datetime')
+    cor_items=('invsvm_ci_svm', 'invsvm_ip')
+    heads = {}
+    params = {}
+    when = {}
+    items = {}
+    for i in in_dict:
+        if i in cor_heads:
+            heads[i] = in_dict[i]
+        elif i in cor_params:
+            params[i] = in_dict[i]
+        elif i in cor_when:
+            when[i] = in_dict[i]
+        elif i in cor_items:
+            items[i] = in_dict[i]
     return [heads, params, when, items]
+def get_params_run():
+    mainparser = argparse.ArgumentParser(description="Arguments for create topics to kafka")
+
+    mainparser.add_argument("-url", dest="url", default='https://rlm.sigma.sbrf.ru/api/tasks/')
+    mainparser.add_argument("-authorization", dest="Authorization", default='token')
+    mainparser.add_argument("-accept", dest="accept", default='application/json')
+    mainparser.add_argument("-content_type", dest="Content-Type", default='application/json')
+
+    mainparser.add_argument("-action", dest="action", default='aktag_create_topic,aktag_modify_topic')
+    mainparser.add_argument("-ak_topic", dest="ak_topic", default='test4api')
+    mainparser.add_argument("-ak_replication_factor", dest="ak_replication_factor", type=int, default=1)
+    mainparser.add_argument("-ak_partitions", dest="ak_partitions", type=int, default=1)
+    mainparser.add_argument("-ak_security", dest="ak_security", default='SSL')
+    mainparser.add_argument("-ak_port", dest="ak_port", type=int, default='9093')
+    mainparser.add_argument("-ak_topic_config", dest="ak_topic_config", default=[])
+    mainparser.add_argument("-user_email", dest="user_email", default='user@example.com')
+    mainparser.add_argument("-env_type", dest="env_type", default='TEST')
+
+    mainparser.add_argument("-start_at", dest="start_at", default='now')
+    mainparser.add_argument("-service", dest="service", default='createtopicKafka')
+    mainparser.add_argument("-datetime", dest="datetime", default=datetime.datetime.utcnow().isoformat())
+
+    mainparser.add_argument("-invsvm_ci_svm", dest="invsvm_ci_svm", default='CI04340141')
+    mainparser.add_argument("-invsvm_ip", dest="invsvm_ip", default='10.56.165.201')
+
+    return split_dict(vars(mainparser.parse_args()))
 
 def post_to_api(all):
     heads = all[0]
@@ -53,10 +62,12 @@ def post_to_api(all):
     body['items'] = buff
     urllib3.disable_warnings()
     json_object = json.dumps(body, indent=4)
-    r = requests.post(url=heads.pop('url'), headers=heads, data=json_object, verify=False)
+    r = requests.post(url=heads.pop('url'), headers=heads, data=json_object, verify=False)    
     print(r.status_code)
     print(r.content)
-    #print(json_object)
-
+    if r.status_code != '200':
+        exit('ERROR: RLM returned an answer not 200')
+    
 if __name__ == '__main__':
+    print(get_params_run())
     post_to_api(get_params_run())
